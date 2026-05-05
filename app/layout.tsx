3846b1f74ea28ec9021c0960438584dec853dd46
jpmartirez <johnpaulmartirez0915@gmail.com>
2026-05-05 16:01:03 +0800

    Integrate Clerk authentication into Navbar and RootLayout; update button styles for primary buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -30,8 +32,10 @@ export default function RootLayout({
 			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 		>
 			<body className="min-h-full flex flex-col">
-				<Navbar />
-				{children}
+				<ClerkProvider>
+					<Navbar />
+					{children}
+				</ClerkProvider>
 			</body>
 		</html>
 	);
